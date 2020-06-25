f1a4089cd6908171a18bbec5deb5541521f33352
Tim Heuer <tim@timheuer.com>
2020-06-25 07:43:41 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace buildtest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
